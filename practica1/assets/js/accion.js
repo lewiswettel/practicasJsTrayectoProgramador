f89a9dc66f126d6d6885f597bdcea9eceed467cc
lewiswettel <lewiswettel1@hotmail.com>
2022-04-22 14:00:41 -0300

    ejercicio 1 base ok

---
+++ practica1/assets/js/accion.js
@@ -0,0 +1,8 @@
+let nombre; 
+nombre= prompt ("Por favor introduzca su nombre");
+
+
+
+let apellido;
+apellido= prompt ("Por favor introduzca su apellido");
+console.log ("tu nombre es " + nombre +" "+ apellido)
